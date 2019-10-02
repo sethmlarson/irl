61adbdb7a87274ba8dbbd5b29721202dad3a15d9
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-10-02 08:24:23 -0500

    Add API stubs for ParsedURL, EncodedURL, and DecodedURL (#2)

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 import re
-import os
+import pathlib
 from setuptools import setup
 
 
-base_dir = os.path.abspath(".")
-with open(os.path.join(base_dir, "irl.py")) as f:
+base_dir = pathlib.Path(__file__).parent
+with open(base_dir / "irl" / "__init__.py") as f:
     version = re.search(r"__version__\s+=\s+\"([\d.]+)\"", f.read()).group(1)
 
 setup(
     name="irl",
     version=version,
     license="MIT",
-    long_description=open(os.path.join(base_dir, "README.md")).read(),
+    long_description=open(base_dir / "README.md", "r").read(),
     long_description_content_type="text/markdown",
     author="Seth Michael Larson",
     author_email="sethmichaellarson@gmail.com",
-    py_modules=["irl"],
+    packages=["irl"],
+    package_data={"irl": ["py.typed"]},
+    include_package_data=True,
+    zip_safe=False,
     python_requires=">=3.6",
-    install_requires=["idna"],
+    install_requires=["idna", "homoglyphs"],
 )
